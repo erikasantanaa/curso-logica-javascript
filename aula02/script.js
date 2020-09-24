976478f6508cc0071718c2acd827a8c17324b662
Erika Santana <erika.araujo12344@gmail.com>
2020-09-24 17:10:48 -0300

    logica com Javascript

--- aula02/script.js
+++
@@ -1,8 +0,0 @@
-//Fazer uma pergunta para o usuario e mostrar uma mensagem de boas vindas.
-
-var nome = prompt("Qual seu nome? ");
-
-alert(`Bem-vindo, ${nome}`);
-
-var endereco = 1.2; //Numero Decimal / Numero Real
-var estaDoente = false; //Boolean
